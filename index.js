cdc55e46c69ad8a76d4db9310f7c65b4b187f12e
Mohammed Ameen <jegroamin@gmail.com>
2018-08-20 18:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange){
   return function(trip){
-    let me = parseInt(trip) - parseInt(blockRange);
+    let me = parseInt(trip) % parseInt(blockRange);
     return `${me} blocks out of range`;
   }
 }
