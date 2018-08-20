faa8dbc0ccc3a8328a273ac1423dc6911242b827
Mohammed Ameen <jegroamin@gmail.com>
2018-08-20 18:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange){
   return function(trip,two){
     let me = parseInt(trip) + parseInt(blockRange);
     let u = parseInt(two);
-    let us = me -u;
+    let us = u -me;
      if(me < u){
        return `${us} blocks out of range`;
      }else{
